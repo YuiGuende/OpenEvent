cc76044f73af73a7ea2d1642cabad1193c876442
YuiGuende <duynguyenrubik968@gmail.com>
2025-09-15 19:10:38 +0700

    first commit

---
+++ src/main/java/com/group02/openevent/OpenEventApplication.java
@@ -0,0 +1,13 @@
+package com.group02.openevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group02/openevent/OpenEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group02.openevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

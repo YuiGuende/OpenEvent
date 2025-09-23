56a1730c757b392d52144ea228acdb8a0e11f221
nguyenquangminhfptu <techminhfptu@gmail.com>
2025-09-23 22:31:55 +0700

    fix trang home thanh cong

--- src/main/java/com/group02/openevent/controller/HomeController.java
+++ src/main/java/com/group02/openevent/controller/HomeController.java
@@ -1,8 +1,15 @@
 package com.group02.openevent.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
-
+    
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("page", "home");
+        return "index"; // templates/index.html
+    }
 }

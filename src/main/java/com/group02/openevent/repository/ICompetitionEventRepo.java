f9d7765f1b671b26b0167ec526786b114e044d68
nguyenquangminhfptu <techminhfptu@gmail.com>
2025-10-02 17:28:35 +0700

    fix footer

--- src/main/java/com/group02/openevent/repository/ICompetitionEventRepo.java
+++
@@ -1,9 +0,0 @@
-package com.group02.openevent.repository;
-
-import com.group02.openevent.model.event.CompetitionEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICompetitionEventRepo extends JpaRepository<CompetitionEvent, Integer> {
-
-}
-

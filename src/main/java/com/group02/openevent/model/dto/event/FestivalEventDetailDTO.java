00546127d1fd27093001b15cd7fa71e2017c49a9
nguyenquangminhfptu <techminhfptu@gmail.com>
2025-10-12 17:54:51 +0700

    add music page sucessfully

--- src/main/java/com/group02/openevent/model/dto/event/FestivalEventDetailDTO.java
+++ src/main/java/com/group02/openevent/model/dto/event/FestivalEventDetailDTO.java
@@ -61,4 +61,236 @@ public class FestivalEventDetailDTO {
 
     // Constructor rỗng
     public FestivalEventDetailDTO() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getBenefits() {
+        return benefits;
+    }
+
+    public void setBenefits(String benefits) {
+        this.benefits = benefits;
+    }
+
+    public String getGuidelines() {
+        return guidelines;
+    }
+
+    public void setGuidelines(String guidelines) {
+        this.guidelines = guidelines;
+    }
+
+    public String getVenueAddress() {
+        return venueAddress;
+    }
+
+    public void setVenueAddress(String venueAddress) {
+        this.venueAddress = venueAddress;
+    }
+
+    public LocalDateTime getStartsAt() {
+        return startsAt;
+    }
+
+    public void setStartsAt(LocalDateTime startsAt) {
+        this.startsAt = startsAt;
+    }
+
+    public LocalDateTime getEndsAt() {
+        return endsAt;
+    }
+
+    public void setEndsAt(LocalDateTime endsAt) {
+        this.endsAt = endsAt;
+    }
+
+    public LocalDateTime getEnrollDeadline() {
+        return enrollDeadline;
+    }
+
+    public void setEnrollDeadline(LocalDateTime enrollDeadline) {
+        this.enrollDeadline = enrollDeadline;
+    }
+
+    public String getBannerUrl() {
+        return bannerUrl;
+    }
+
+    public void setBannerUrl(String bannerUrl) {
+        this.bannerUrl = bannerUrl;
+    }
+
+    public List<String> getGalleryUrls() {
+        return galleryUrls;
+    }
+
+    public void setGalleryUrls(List<String> galleryUrls) {
+        this.galleryUrls = galleryUrls;
+    }
+
+    public String getCulture() {
+        return culture;
+    }
+
+    public void setCulture(String culture) {
+        this.culture = culture;
+    }
+
+    public String getHighlight() {
+        return highlight;
+    }
+
+    public void setHighlight(String highlight) {
+        this.highlight = highlight;
+    }
+
+    public String getFestivalTheme() {
+        return festivalTheme;
+    }
+
+    public void setFestivalTheme(String festivalTheme) {
+        this.festivalTheme = festivalTheme;
+    }
+
+    public Integer getNumberOfActivities() {
+        return numberOfActivities;
+    }
+
+    public void setNumberOfActivities(Integer numberOfActivities) {
+        this.numberOfActivities = numberOfActivities;
+    }
+
+    public String getTargetAudience() {
+        return targetAudience;
+    }
+
+    public void setTargetAudience(String targetAudience) {
+        this.targetAudience = targetAudience;
+    }
+
+    public Boolean getRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(Boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
+    }
+
+    public String getFestivalType() {
+        return festivalType;
+    }
+
+    public void setFestivalType(String festivalType) {
+        this.festivalType = festivalType;
+    }
+
+    public String getMainOrganizer() {
+        return mainOrganizer;
+    }
+
+    public void setMainOrganizer(String mainOrganizer) {
+        this.mainOrganizer = mainOrganizer;
+    }
+
+    public Integer getExpectedAttendees() {
+        return expectedAttendees;
+    }
+
+    public void setExpectedAttendees(Integer expectedAttendees) {
+        this.expectedAttendees = expectedAttendees;
+    }
+
+    public List<SpeakerDTO> getSpeakers() {
+        return speakers;
+    }
+
+    public void setSpeakers(List<SpeakerDTO> speakers) {
+        this.speakers = speakers;
+    }
+
+    public List<ScheduleDTO> getSchedules() {
+        return schedules;
+    }
+
+    public void setSchedules(List<ScheduleDTO> schedules) {
+        this.schedules = schedules;
+    }
+
+    public Map<LocalDate, List<ScheduleDTO>> getSchedulesByDay() {
+        return schedulesByDay;
+    }
+
+    public void setSchedulesByDay(Map<LocalDate, List<ScheduleDTO>> schedulesByDay) {
+        this.schedulesByDay = schedulesByDay;
+    }
+
+    public List<PlaceDTO> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<PlaceDTO> places) {
+        this.places = places;
+    }
+
+    public List<TicketTypeDTO> getTicketTypes() {
+        return ticketTypes;
+    }
+
+    public void setTicketTypes(List<TicketTypeDTO> ticketTypes) {
+        this.ticketTypes = ticketTypes;
+    }
+
+    public OrganizationDTO getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(OrganizationDTO organization) {
+        this.organization = organization;
+    }
+
+    public EventStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(EventStatus status) {
+        this.status = status;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(EventType eventType) {
+        this.eventType = eventType;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }

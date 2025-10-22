587d003ccc72e23f695462b66aeecc0a9276c445
YuiGuende <duynguyenrubik968@gmail.com>
2025-10-22 13:49:23 +0700

    update test dependency

---
+++ src/test/java/com/group02/openevent/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.group02.openevent.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrderWithTicketTypes() {
+    }
+
+    @Test
+    void cancelOrder() {
+    }
+
+    @Test
+    void confirmOrder() {
+    }
+}

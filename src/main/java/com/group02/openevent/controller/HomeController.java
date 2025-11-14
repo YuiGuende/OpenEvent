62e41463568f6411b01004f643f1a5cb4caac7a6
YuiGuende <duynguyenrubik968@gmail.com>
2025-11-14 10:39:02 +0700

    a

--- src/main/java/com/group02/openevent/controller/HomeController.java
+++ src/main/java/com/group02/openevent/controller/HomeController.java
@@ -27,6 +27,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
